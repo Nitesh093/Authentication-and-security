1fb2ff91bc7d9e7be239ae643fce583b333d4b82
nitesh kumawat <kumawatnitjee@gmail.com>
2023-02-10 00:39:48 +0530

    level 4 bcrypt hash

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //jshint esversion:6
 require('dotenv').config()
-const md5=require('md5')
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const ejs=require('ejs');
 const express=require('express');
 const bodyParser=require('body-parser');
@@ -41,36 +42,44 @@ app.get('/register',function(req,res){
 })
 
 app.post('/register',function(req,res){
-    const NewUser=new User({
-        username:req.body.username,
-        password:md5(req.body.password)
-    })
-    NewUser.save(function(err){
-        if(err){
-            console.log(err)
-        }else{
-        console.log("succesful register")
-        }
-    })
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const NewUser=new User({
+            username:req.body.username,
+            password:hash
+        })
+        NewUser.save(function(err){
+            if(err){
+                console.log(err)
+            }else{
+            console.log("succesful register")
+            }
+        })
+    });
+    
     res.render('secrets');
 
 })
 
 app.post('/login',function(req,res){
     const Name=req.body.username;
-    const pass=md5(req.body.password);
+    const pass=req.body.password;
     User.find({username:Name},function(err,data){
         if(err){
             console.log(err);
         }
         else{
-           
-        if(data[0].password==pass){
-            res.render('secrets');
-        }
+            bcrypt.compare(pass, data[0].password, function(err, result) {
+                // result == true
+                res.render('secrets');
+            }); 
+            
+       
     }
 
     })
+    
+    
     
     
 })
