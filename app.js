3d498c2cead24d9cc964da7542f68aa3b7748f6e
nitesh kumawat <kumawatnitjee@gmail.com>
2023-02-09 23:36:25 +0530

    level 3 hasing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 require('dotenv').config()
+const md5=require('md5')
 const ejs=require('ejs');
 const express=require('express');
 const bodyParser=require('body-parser');
@@ -19,7 +20,7 @@ const userSchema=new mongoose.Schema({
     password:String
 })
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User=mongoose.model("User",userSchema);
 
@@ -42,7 +43,7 @@ app.get('/register',function(req,res){
 app.post('/register',function(req,res){
     const NewUser=new User({
         username:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     })
     NewUser.save(function(err){
         if(err){
@@ -57,14 +58,13 @@ app.post('/register',function(req,res){
 
 app.post('/login',function(req,res){
     const Name=req.body.username;
-    const pass=req.body.password;
+    const pass=md5(req.body.password);
     User.find({username:Name},function(err,data){
         if(err){
             console.log(err);
         }
         else{
-            console.log(data[0].username);
-            console.log(data[0].password);
+           
         if(data[0].password==pass){
             res.render('secrets');
         }
